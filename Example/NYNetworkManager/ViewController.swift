3ace26700c4760a8470c784e8a3d596b714fbde5
yu nie <nieyuchina@gmail.com>
2020-07-06 21:10:08 +0800

    update pod spec

--- Example/NYNetworkManager/ViewController.swift
+++ Example/NYNetworkManager/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import NYSwiftCommon
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        printABC.printABC()
     }
 
     override func didReceiveMemoryWarning() {
